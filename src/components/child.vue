5405615f0e129ea631f36abbef83b94cfe303569
Rhythm-08 <rhythm.sharma@chicmic.co.in>
2023-06-18 16:25:13 +0530

    Passing Data from Parent to Child Component

---
+++ src/components/child.vue
@@ -0,0 +1,18 @@
+<template>
+<h2>Child Component</h2>
+<h3>{{ name }}</h3>
+<h3>{{user.email}}</h3>
+<button v-on:click="getData()">Call Function</button>
+</template>
+
+<script>
+export default {
+    name:'ChildComponent',
+    props:{
+        name:String ,
+        user:Object,
+        getData:Function
+    }
+}
+</script>
+
--- src/components/home.vue
+++ src/components/home.vue
@@ -5,21 +5,34 @@
    <input type="password" placeholder="enter password" v-model="password"/>
    <br><br><br>
    <button v-on:click="onSubmit()" type="button">Get Values</button>
+   <child name="Rhythm Sharma" :user="user" :getData="getData"/>
 </template>
 
 <script>
+    import child from './child.vue'
+
 export default {
+    components:{
+        child
+    },
     name:'homePage',
     data(){
         return{
             email:null,
             password:null,
+            user:{
+                name:'Peter',
+                email:'peterQuill@gmail.com'
+            }
 
         }
     },
     methods:{
         onSubmit(){
              console.warn('Values', this.email , this.password)
+        },
+        getData(){
+            console.log('Parent function Called')
         }
     }
     
